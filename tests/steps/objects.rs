58d50526754318b881347ca348c0f7866275172c
David Sauer <davedamoon@gmail.com>
2021-11-04 21:37:57 +0100

    migrate tests to cucumber 0.10

--- tests/steps/objects.rs
+++ tests/steps/objects.rs
@@ -4,37 +4,35 @@ use crate::{
     steps::tuples::{parse_point, parse_vector},
     MyWorld,
 };
-use cucumber::Steps;
+use cucumber::{given, then, when};
 use lab_raytracing_rs::objects::default_testshape;
 
-pub fn steps() -> Steps<MyWorld> {
-    let mut steps: Steps<MyWorld> = Steps::new();
+#[given(regex = r"^(s) ← test_shape\(\)$")]
+async fn create_test_shape(world: &mut MyWorld, shape: String) {
+    world.objects.insert(shape, Arc::new(default_testshape()));
+}
 
-    steps.given_regex(r#"^(s) ← test_shape\(\)$"#, |mut world, ctx| {
-        world
-            .objects
-            .insert(ctx.matches[1].clone(), Arc::new(default_testshape()));
-        world
-    });
-
-    steps.then_regex(
-        r#"^s.saved_ray.(origin|direction) = (point|vector)\(([-0-9.]+), ([-0-9.]+), ([-0-9.]+)\)$"#,
-        |world, ctx| {
-            let desired = match ctx.matches[2].as_str() {
-                "point" => parse_point(&ctx.matches[3..=5]),
-                "vector" => parse_vector(&ctx.matches[3..=5]),
-                _ => panic!("desired kind not covered"),
-            };
-            let ray = lab_raytracing_rs::objects::SAVED_RAY.with(|c| c.read().unwrap().clone());
-            let lookup = match ctx.matches[1].as_str() {
-                "origin" => ray.origin.clone(),
-                "direction" => ray.direction.clone(),
-                _ => panic!("lookup attribute not covered"),
-            };
-            assert_eq!(lookup, desired);
-            world
-        },
-    );
-
-    steps
+#[then(
+    regex = r"^s.saved_ray.(origin|direction) = (point|vector)\(([-0-9.]+), ([-0-9.]+), ([-0-9.]+)\)$"
+)]
+async fn compare_ray_properties(
+    _world: &mut MyWorld,
+    property: String,
+    kind: String,
+    x: String,
+    y: String,
+    z: String,
+) {
+    let desired = match kind.as_str() {
+        "point" => parse_point(&[x, y, z]),
+        "vector" => parse_vector(&[x, y, z]),
+        _ => panic!("desired kind not covered"),
+    };
+    let ray = lab_raytracing_rs::objects::SAVED_RAY.with(|c| c.read().unwrap().clone());
+    let lookup = match property.as_str() {
+        "origin" => ray.origin.clone(),
+        "direction" => ray.direction.clone(),
+        _ => panic!("lookup attribute not covered"),
+    };
+    assert_eq!(lookup, desired);
 }
